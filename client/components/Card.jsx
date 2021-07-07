0eaf5cb3b4f9c511cd2aa6259480ce96d13d4479
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-07-07 13:22:12 -0400

    add dark mode to cards

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image, useColorModeValue } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
 import { FaDiscord, FaWhatsapp } from "react-icons/fa";
@@ -6,20 +6,37 @@ import Tilt from "react-vanilla-tilt";
 
 export const Card = ({ name, description, links, id }) => {
   const { locale, defaultLocale, push } = useRouter();
-
+  const backgroundColor = useColorModeValue("#FFFFFF", "#181a1b");
   const LinksMapped = links.map((link) => {
     if (link.includes("discord")) {
       return { type: "discord" };
     }
     return { type: "whatsapp" };
   });
+
+  const cardStyle = {
+    background: backgroundColor,
+    width: "300px",
+    padding: "30px",
+    margin: "10px",
+    borderRadius: "4px; color: rgb(54, 73, 98);",
+    fontSize: "16px; line-height: 1.6;",
+  };
+
   return (
     <Tilt
       onClick={() => {
         push(`${locale !== defaultLocale ? locale : ""}/chat/${id}`);
       }}
+      style={cardStyle}
     >
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Box
+        bg={backgroundColor}
+        maxW="sm"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+      >
         <Box justifyContent="center">
           <Image
             src="https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=400&fit=max"
