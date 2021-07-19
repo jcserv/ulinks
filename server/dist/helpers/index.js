1459eeb2f873e37348515b14ca0891a9c9ae0969
Jarrod Servilla <45128231+jcserv@users.noreply.github.com>
2021-07-19 13:35:55 -0400

    V0.98 (#53)
    * bug/admin-request-autoapprove (#52)
    
    * Admin request auto-approve
    
    * Removed .idea directory
    
    * fix variable id length (#51)
    
    Co-authored-by: Yousef <yousefbulbulia1@gmail.com>
    Co-authored-by: Ritvik Bhardwaj <43301959+AipioxTechson@users.noreply.github.com>

--- server/dist/helpers/index.js
+++ server/dist/helpers/index.js
@@ -7,7 +7,7 @@ function escapeRegex(text) {
 exports.escapeRegex = escapeRegex;
 function generateRandomString() {
     return (Math.random().toString(36).substring(2, 15) +
-        Math.random().toString(36).substring(2, 15));
+        Math.random().toString(36).substring(2, 15)).slice(0, 20);
 }
 exports.generateRandomString = generateRandomString;
 exports.departmentToImage = {
