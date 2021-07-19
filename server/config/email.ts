16db4ee87fed4c77a27bed14b1e2650ffe6ceaf7
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-07-18 21:24:42 -0400

    cut my life into pieces

--- server/config/email.ts
+++ server/config/email.ts
@@ -1,25 +1,21 @@
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
-const { OAuth2 } = google.auth;
 
-const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
-
-const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN } = process.env;
-const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, OAUTH_PLAYGROUND);
-oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, ACCESS_TOKEN } = process.env;
 
 export const getTransporter = async () => {
   if (process.env.NODE_ENV === "production") {
-    const accessToken = await oauth2Client.getAccessToken();
     return nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         type: "OAuth2",
         user: process.env.NODEMAILER_EMAIL,
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
-        accessToken: accessToken as string,
+        accessToken: ACCESS_TOKEN,
+        expires: 3599,
       },
     });
   }
--- server/dist/config/email.js
+++ server/dist/config/email.js
@@ -5,24 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getTransporter = void 0;
 const nodemailer_1 = __importDefault(require("nodemailer"));
-const googleapis_1 = require("googleapis");
-const { OAuth2 } = googleapis_1.google.auth;
-const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
-const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN } = process.env;
-const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, OAUTH_PLAYGROUND);
-oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, ACCESS_TOKEN } = process.env;
 const getTransporter = async () => {
     if (process.env.NODE_ENV === "production") {
-        const accessToken = await oauth2Client.getAccessToken();
         return nodemailer_1.default.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
             auth: {
                 type: "OAuth2",
                 user: process.env.NODEMAILER_EMAIL,
                 clientId: CLIENT_ID,
                 clientSecret: CLIENT_SECRET,
                 refreshToken: REFRESH_TOKEN,
-                accessToken: accessToken,
+                accessToken: ACCESS_TOKEN,
+                expires: 3599,
             },
         });
     }
