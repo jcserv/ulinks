abb3c1b5836977f280e647fab1059f40640bd754
Jarrod Servilla <45128231+jcserv@users.noreply.github.com>
2021-07-24 15:53:54 -0400

    🚀 Feature Request: Filter Groupchat by Relevance/Hotness  (#69)
    * increment view count on page load + sort by views
    
    * add likes incrementation + filtering
    
    * combine sorts into one call

--- server/index.ts
+++ server/index.ts
@@ -16,6 +16,7 @@ import {
 mongoose.connect(`${process.env.MONGO_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const customAuthChecker: AuthChecker<any> = (
